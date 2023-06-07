5c012ecb05400a249a200c11fa2cc0ec0f71b011
Satyam kumar <satyam829122@gmail.com>
2023-06-07 21:48:47 +0530

    Logical operator

---
+++ js4.js
@@ -0,0 +1,12 @@
+// OR LOGICAL OPERATOR
+
+// it checks for at least one condition to be true.
+
+var i = 7;
+var j = 2;
+if(i < 7 || j < 5) {
+  console.log("Hello");
+} else {
+  console.log("Goodbye");
+}
+
