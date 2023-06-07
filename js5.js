0881644bfc9a6418c02ac90b0174bb7981d0873e
Satyam kumar <satyam829122@gmail.com>
2023-06-07 21:48:58 +0530

    Function

---
+++ js5.js
@@ -0,0 +1,17 @@
+//FUNCTION
+
+// Function without parameters
+function addtwonums(){
+    var a = 5
+    var b = 5
+    var c = a + b
+    console.log(c)
+}
+// addtwonums() can be used to call the function
+
+// Function with parameter
+function addtwonumbers(a,b){
+    var c = a + b
+    console.log(c)
+}
+// addtwonumbers(4,5) can be used to recall the function
