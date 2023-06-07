11cef3652d968ff52455cc363d7b3d759a9fbffe
Satyam kumar <satyam829122@gmail.com>
2023-06-07 21:49:40 +0530

    Objects

---
+++ js7.js
@@ -0,0 +1,11 @@
+// OBJECTS
+
+var storemanager = {}
+storemanager.rangetilesperturn = 4
+storemanager.socialskills = 40
+storemanager.streetsmart = 30
+storemanager.health = 30
+storemanager.specialability = "finding business opportunities"
+storemanager.greeting = "Let's make some money.."
+
+/*in objects the variable name(ex- socialskills) becomes property key and variable value(ex- 50) becomes property value*/
