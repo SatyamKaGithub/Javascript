1f8bb2c59c7e6a8677b3d17d76401509b6aef9d0
Satyam kumar <satyam829122@gmail.com>
2023-06-07 21:48:07 +0530

    For loop

---
+++ js3.js
@@ -0,0 +1,17 @@
+// FOR LOOP
+
+// Repeats until a specified condition evaluates to false.
+
+for (var i=0; i<=3; i++){
+    console.log(i)
+}
+
+// WHILE LOOP
+
+// Creates a loop (around a code block) that is executed while a condition is true.
+
+var i = 1;
+while (i<100){
+    console.log(i)
+    i=i+1
+}
