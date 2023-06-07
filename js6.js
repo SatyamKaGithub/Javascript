620d114f1b48ade5e9da8c00474b5595f127e794
Satyam kumar <satyam829122@gmail.com>
2023-06-07 21:49:14 +0530

    Objects

---
+++ js6.js
@@ -0,0 +1,14 @@
+// ARRAYS
+/* values in an array are all part of a group.
+values are set in a specific sequence.
+values can be accessed with ther index number.*/
+
+// suppose there are two cargo train carrying different things. We can use array to find out which bogey is carrying what item.
+// suppose train 1 has 5 bogey and train 2 has 2 bogeys.
+
+var train1 = ["wheat" , "barley" , "oats" , "potatoes" , "onion"]
+var train2 = ["soda" , "coke" ]
+// array automatically assign numbers to the bogeys(0,1,2,3,4,.....)
+console.log(train1[4])
+console.log(train1[0])
+console.log(train2[1])
