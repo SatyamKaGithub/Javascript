7527f10a2a33f0923bf6074ffc074241e4993b89
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-09 18:25:05 +0530

    POP and CONCAT

---
+++ js12.js
@@ -0,0 +1,14 @@
+/* POP is used to remove an item from arrays
+
+the CONCAT method accept whatever value you want to concat or join.*/
+
+var clothes = []
+clothes.push('shirt')
+clothes.push("joggers")
+clothes.push('chinos')
+clothes.push('bikinis')
+clothes.pop()
+clothes.push('Air jordan')
+clothes.push('Suiiiiiiii')
+console.log(clothes)
+console.log(clothes[3])
