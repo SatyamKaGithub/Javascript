1fd7b4e5ed74a3b0e4f1a08166e76fd56793951c
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-09 18:25:35 +0530

    typeof Operator

---
+++ js13.js
@@ -0,0 +1,15 @@
+// typeof OPERATOR
+
+
+//(you can remove comments on each line one by one to check the typeof operator)
+
+
+
+//var test = typeof("what's this")
+//var test = typeof(10)
+//var test = typeof(3.14)
+//var test = typeof(true)
+//var test = typeof(false)
+//var test = typeof(1 < 2)
+//var test = typeof(function abc(){console.log('abc')})
+console.log(test)
