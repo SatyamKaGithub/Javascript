260016112987887b559d46fdfa97c3d3eb472144
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-09 18:24:09 +0530

    Arrays are utterable

---
+++ js10.js
@@ -0,0 +1,9 @@
+// Arrays are iterable
+
+var veggies = ['onion' , 'potatoes' , 'tomatoes']
+console.log(veggies.length)
+console.log(veggies[0])
+console.log(veggies[1])
+for (var i = 0; i < veggies.length; i++){
+    console.log(veggies[i])
+}
