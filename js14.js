dfe4276828260b1464309f33343e059250b4628e
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-10 23:10:30 +0530

    Bugs

---
+++ js14.js
@@ -0,0 +1,13 @@
+// BUGS
+// A bug causes a program to run in an unintended way.
+
+function addnums (a , b){
+     console.log(a+b)
+}
+    addnums("1" , 2)
+    console.log("still running")
+    // in this example 1 is used as a string and 2 as a number. 
+    //The code is giving the output in string and it is not doing the addition.
+
+
+// so in bug your code will run but it will not give the desirable outpu.
