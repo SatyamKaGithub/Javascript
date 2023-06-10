3019c65aaf8f22d6ff2f0d65b884d12856b6df83
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-10 23:59:50 +0530

    Scope

---
+++ js21.js
@@ -0,0 +1,8 @@
+// SCOPE
+
+// Scope in javascript works similar to a two way mirror glass
+
+var globalVar = "I'm in the global scope";
+function scopeTest () {
+var localVar = "I'm scoped to the function";
+}
