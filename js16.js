6bc703edb96efca013cd61b845309167987bf92d
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-10 23:11:07 +0530

    Try

---
+++ js16.js
@@ -0,0 +1,10 @@
+// TRY
+
+try{
+    console.log(a+b)
+} catch(err){
+    console.log(err)
+    console.log('There was an Error')
+    console.log('The error was saved to the error log')
+}
+console.log("MY PROGRAM DOES NOT STOP")
