9d4446de81f4ac72cd927d2151f05a37a7d2d65e
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-10 23:10:50 +0530

    Errors

---
+++ js15.js
@@ -0,0 +1,15 @@
+// ERRORS
+
+// An error causes a program to stop running.
+// Error can be defined as a faulty piece of code that prevent program for further execution.
+// Errors are of many types like Syntax error , Type error , Reference error
+
+// Syntax error
+var world = "hello;
+
+//Type error
+(5).prop;
+
+// Reference error
+console.log(c+d)
+console.log('this line will never run')
