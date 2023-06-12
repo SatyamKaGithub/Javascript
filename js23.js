a6c0f23c1d22b2f1e06891d4344bbceb4abcf00b
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-12 14:14:44 +0530

    VAR

---
+++ js23.js
@@ -0,0 +1,9 @@
+// VAR keyword
+
+// We can access a var variable before initializing as long as the var variable is Eventually Initialized anywhere in the code.
+// var ca be redeclared
+ 
+var user = "Jai"
+var user = "Shree"
+var user = "Ram"
+console.log(user) 
