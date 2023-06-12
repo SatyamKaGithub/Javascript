1350de00fd03b04eccfdac80da3e67b2f53c2ded
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-12 14:15:20 +0530

    CONST

---
+++ js25.js
@@ -0,0 +1,8 @@
+// CONST keyword 
+
+// The const variable must be initialized 
+// We can't access the const variable before initialization
+// We can't redeclare a const variable
+
+const user = "satyam"
+console.log(user)
