05538cfaf302e4db38217b17d41dea11f5dfb5cc
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-12 14:15:00 +0530

    LET

---
+++ js24.js
@@ -0,0 +1,11 @@
+// LET keyword
+
+// We cannot access a let variable before we declare it
+// We can declare an unassigned variable with let
+// We can't redeclare a let variable
+// We can reassign a variable
+
+let user;
+console.log(user) // here it will give undefined output
+user = "satyam"
+console.log(user)
