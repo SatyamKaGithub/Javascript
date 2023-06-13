c028bd75879fe4a2aa8d15dc4c31d26ae1240637
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-13 13:10:14 +0530

    Objects method

---
+++ js33.js
@@ -0,0 +1,11 @@
+// OBJECTS METHOD
+
+
+const car3 = {
+    speed: 300,
+    color: "black",
+    engine: "v16"
+}
+console.log(Object.keys(car3))               // Object.keys() method
+console.log(Object.values(car3))             // Object.valur() method
+console.log(Object.entries(car3))            // Object.entries() method
