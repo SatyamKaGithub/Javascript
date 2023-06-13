dab3f1647af3d07d9c935c0ffb02000c20da3278
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-13 13:12:24 +0530

    Multi line template literal

---
+++ js37.js
@@ -0,0 +1,16 @@
+// ES6 Multi line template literals
+
+let multiLine = `
+    using ES6
+    backticks
+    multi line
+    strings are
+    possible
+    `;
+console.log(multiLine);
+
+// ES6 variable interpolation
+
+let first = `He said, "Don't you know? ES6, it's got some great feature!"`;
+let second = `"Wouldn't you want to learn more?, he asked."`;
+console.log(`${first} - and I got curious. ${second}`);
