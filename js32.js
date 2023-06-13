72cc0c30142f1f5fb57361b8a5d02642dee1b87c
Satyam kumar <114273646+SatyamKaGithub@users.noreply.github.com>
2023-06-13 13:09:47 +0530

    De-structuring arrays and objects

---
+++ js32.js
@@ -0,0 +1,11 @@
+// De-structuring arrays and objects
+
+// Copying an item, which then becomes independent of the original.
+
+let {PI} = Math;
+console.log(PI)
+
+
+let {pi} = Math;            // lowercase pi is not available in the Math obect 
+console.log(pi)
+
